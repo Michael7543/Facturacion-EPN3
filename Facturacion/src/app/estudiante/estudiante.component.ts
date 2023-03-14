bf2232a88b9cc8e26362a342abb93d2a5bc3cb4b
michael7543 <mdr.moran@yavirac.edu.ec>
2023-03-14 08:55:19 -0500

    xd

--- Facturacion/src/app/estudiante/estudiante.component.ts
+++ Facturacion/src/app/estudiante/estudiante.component.ts
@@ -22,7 +22,7 @@ export class EstudianteComponent implements OnInit {
 
   ngOnInit(): void {
     this.cargardatos;
-    this.getEstudiantes2();
+    this.getEstudiantes();
 
     
     
@@ -38,22 +38,30 @@ export class EstudianteComponent implements OnInit {
     console.log(this.estudiante);
   }
 
-  getEstudiantes(){
+  /* getEstudiantes(){
     this.estudiantesService.getAll3().subscribe(response=>{
       this.estudiante = response;
       console.log(this.estudiante)
 
     })
-  }
-  getEstudiantes2() {
-    const obs1 = this.estudiantesService.getAll();
+  } */
+
+  getEstudiantes() {
+    const obs1 = this.estudiantesService.getAll1();
     const obs2 = this.estudiantesService.getAll2();
     const obs3 = this.estudiantesService.getAll3();
     const obs4 = this.estudiantesService.getAll4();
+    const obs5 = this.estudiantesService.getAll5();
+    const obs6 = this.estudiantesService.getAll6();
+    const obs7 = this.estudiantesService.getAll7();
+    const obs8 = this.estudiantesService.getAll8();
+    const obs9 = this.estudiantesService.getAll9();
+    const obs10 = this.estudiantesService.getAll10();
+    const obs11 = this.estudiantesService.getAll11();
   
   
-    forkJoin([obs1, obs2, obs3,obs4]).subscribe(([res1, res2, res3,res4]) => {
-      this.estudiante = [...res1, ...res2, ...res3, ...res4];
+    forkJoin([obs1, obs2, obs3,obs4,obs5,obs6,obs7,obs8,obs9,obs10,obs11]).subscribe(([res1, res2, res3,res4,res5,res6,res7,res8,res9,res10,res11]) => {
+      this.estudiante = [...res1, ...res2, ...res3, ...res4, ...res5, ...res6, ...res7, ...res8, ...res9, ...res10, ...res11];
       console.log(this.estudiante);
     });
   } 
--- Facturacion/src/app/services/estudiante-http.service.ts
+++ Facturacion/src/app/services/estudiante-http.service.ts
@@ -12,37 +12,87 @@ export class EstudianteHttpservice {
 
   estudiante: EstudianteModel[] = [];
 
-   readonly URL_API: string =
-   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0501553408/2022/2';
-
-  readonly URL_API2: string =
-   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1719387852/2022/2';
-
-  readonly URL_API3: string =
-   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0102007176/2022/2';
-
-  readonly URL_API4: string =
-   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0102007176/2022/2';
-
- private readonly URL_API5: string =
+  readonly URL_API: string =
    'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/${cedula}/2022/2';
 
+  readonly URL_API1: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0101827806/2022/2';
+
+  readonly URL_API2: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0502951403/2022/2';
+
+  readonly URL_API3: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1725022329/2022/2';
+
+  readonly URL_API4: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1712622164/2022/2';
+
+  readonly URL_API5: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1802467793/2022/2';
+
+  readonly URL_API6: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1719387852/2022/2';
+
+  readonly URL_API7: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1717677007/2022/2';
+
+  readonly URL_API8: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1756943757/2022/2';
+
+  readonly URL_API9: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0703844696/2022/2';
+
+  readonly URL_API10: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/0102007176/2022/2';
+
+  readonly URL_API11: string =
+   'http://172.31.5.24:8080/WSSIIEPN/webresources/SilaboEPN/carga/1706280243/2022/2';
 
 
-  getAll(): Observable<EstudianteModel[]> {
-    return this.httpClient.get<EstudianteModel[]>(this.URL_API);
+  
+
+
+
+
+
+  getAll1(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API1);
   }
   getAll2(): Observable<EstudianteModel[]> {
     return this.httpClient.get<EstudianteModel[]>(this.URL_API2);
   }
+  getAll3(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API3);
+  }
   getAll4(): Observable<EstudianteModel[]> {
     return this.httpClient.get<EstudianteModel[]>(this.URL_API4);
+  }
+  getAll5(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API5);
+  }
+  getAll6(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API6);
+  }
+  getAll7(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API7);
+  }
+  getAll8(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API8);
+  }
+  getAll9(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API9);
+  }
+  getAll10(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API10);
+  }
+  getAll11(): Observable<EstudianteModel[]> {
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API11);
   }
 
 
   
 
-  getAll3(): Observable<EstudianteModel[]> {
+/*   getAll3(): Observable<EstudianteModel[]> {
     const obs1 = this.httpClient.get<EstudianteModel[]>(this.URL_API);
     const obs2 = this.httpClient.get<EstudianteModel[]>(this.URL_API2);
     const obs3 = this.httpClient.get<EstudianteModel[]>(this.URL_API3);
@@ -54,12 +104,12 @@ export class EstudianteHttpservice {
         return combinedResult;
       })
     );
-  } 
+  }  */
 
 
 
   getone(cedula:string): Observable<EstudianteModel[]>{
-    return this.httpClient.get<EstudianteModel[]>(this.URL_API5.replace('${cedula}',cedula))
+    return this.httpClient.get<EstudianteModel[]>(this.URL_API.replace('${cedula}',cedula))
   }
 
 
